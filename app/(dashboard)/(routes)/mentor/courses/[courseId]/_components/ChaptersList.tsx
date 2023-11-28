16ce4a0203a9b08d9650b36c8ae077ec35ec9027
fensken <shardul.gautam123@gmail.com>
2023-11-28 10:23:25 +0545

    feat: added chapters lists and functionality.

---
+++ app/(dashboard)/(routes)/mentor/courses/[courseId]/_components/ChaptersList.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { FC, useEffect, useState } from "react";
+import { Chapter } from "@prisma/client";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "@hello-pangea/dnd";
+import { Grip, Pencil } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+
+interface ChaptersListProps {
+  items: Chapter[];
+  onReorder: (updateData: { id: string; position: number }[]) => void;
+  onEdit: (id: string) => void;
+}
+
+const ChaptersList: FC<ChaptersListProps> = ({ items, onReorder, onEdit }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [chapters, setChapters] = useState(items);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    setChapters(items);
+  }, []);
+
+  const onDragEnd = (result: DropResult) => {
+    // checks if the drag event was outside or invalid
+    if (!result.destination) return;
+
+    // creates copy of array to check for the reordered item
+    // extracts the item and places it at destination position
+    const items = Array.from(chapters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    // checks the start and end index of items that were effected in the drag process
+    const startIndex = Math.min(result.source.index, result.destination.index);
+    const endIndex = Math.max(result.source.index, result.destination.index);
+
+    // creates only the array of items that changed their positions
+    const updatedChapters = items.slice(startIndex, endIndex + 1);
+
+    setChapters(items);
+
+    // created an array of updatedChapters with their ids and new position (for payload)
+    const bulkUpdateData = updatedChapters.map((chapter) => ({
+      id: chapter.id,
+      position: items.findIndex((item) => item.id === chapter.id),
+    }));
+
+    onReorder(bulkUpdateData);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="chapters">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {chapters.map((chapter, index) => (
+              <Draggable
+                key={chapter.id}
+                draggableId={chapter.id}
+                index={index}
+              >
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    className={cn(
+                      "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                      chapter.isPublished &&
+                        "bg-sky-200 border-sky-200 text-sky-700"
+                    )}
+                    {...provided.draggableProps}
+                  >
+                    <div
+                      className={cn(
+                        "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                        chapter.isPublished &&
+                          "border-r-sky-200 hover:bg-sky-200"
+                      )}
+                      {...provided.dragHandleProps}
+                    >
+                      <Grip className="h-5 w-5" />
+                    </div>
+                    {chapter.title}
+                    <div className="ml-auto pr-2 flex items-center gap-x-2">
+                      {chapter.isFree && <Badge>Free</Badge>}
+                      <Badge
+                        className={cn(
+                          "bg-slate-500",
+                          chapter.isPublished && "bg-sky-700"
+                        )}
+                      >
+                        {chapter.isPublished ? "Published" : "Draft"}
+                      </Badge>
+                      <Pencil
+                        onClick={() => onEdit(chapter.id)}
+                        className="w-4 h-4 cursor-pointer hover:opacity-75 transition"
+                      />
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default ChaptersList;
