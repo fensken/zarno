aeba74a1efd66ff39040bd6b872fdcedd2224efe
fensken <shardul.gautam123@gmail.com>
2024-01-10 09:45:40 +0545

    feat: few changes on chapters page.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { getChapter } from "@/actions/getChapter";
 
 import Banner from "@/components/Banner";
+import VideoPlayer from "./_components/VideoPlayer";
 
 type pageProps = {
   params: {
@@ -52,6 +53,20 @@ const page = async ({ params }: pageProps) => {
           label="You need to prurchase this course to watch this chapeter."
         />
       )}
+
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+      </div>
     </div>
   );
 };
