8830bedf088a79801539e6da344dd7bd9e62e056
fensken <shardul.gautam123@gmail.com>
2024-02-17 18:30:19 +0545

    fix: minor build errors.

--- app/(dashboard)/(routes)/mentor/courses/[courseId]/_components/CategoryForm.tsx
+++ app/(dashboard)/(routes)/mentor/courses/[courseId]/_components/CategoryForm.tsx
@@ -105,7 +105,7 @@ const CategoryForm: FC<CategoryFormProps> = ({
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Combobox options={...options} {...field} />
+                    <Combobox options={options} {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
