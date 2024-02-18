3fd3e0873afe63c2b57ebcf34fbbcc9046059be9
fensken <shardul.gautam123@gmail.com>
2024-02-18 16:12:08 +0545

    feat: added all to category.

--- app/(dashboard)/(routes)/browse/_components/Categories.tsx
+++ app/(dashboard)/(routes)/browse/_components/Categories.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { FC } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Category } from "@prisma/client";
 import {
+  FcDatabase,
   FcBiotech,
   FcEngineering,
   FcFilmReel,
@@ -14,6 +16,7 @@ import {
 } from "react-icons/fc";
 import { IconType } from "react-icons";
 
+import { cn } from "@/lib/utils";
 import CategoryItem from "./CategoryItem";
 
 type CategoriesProps = {
@@ -32,21 +35,41 @@ const iconMap: Record<Category["name"], IconType> = {
 };
 
 const Categories: FC<CategoriesProps> = ({ items }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const currentCategoryId = searchParams.get("categoryId");
+
   return (
-    <div className="flex items-center pb-2 overflow-x-auto gap-x-2">
-      {items.map((item) => {
-        return (
-          <>
+    <>
+      <div className="flex items-center pb-2 overflow-x-auto gap-x-2">
+        {/* All category button */}
+        <button
+          onClick={() => {
+            router.push("/");
+          }}
+          className={cn(
+            "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",
+            !currentCategoryId && "border-sky-700 bg-sky-200/20 text-sky-800"
+          )}
+        >
+          <FcDatabase size={20} />
+          <div className="truncate">All</div>
+        </button>
+
+        {/* Other category buttons */}
+        {items.map((item) => {
+          return (
             <CategoryItem
               key={item.id}
               label={item.name}
               icon={iconMap[item.name]}
               value={item.id}
             />
-          </>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
